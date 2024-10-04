e26ef4f85a9c62a3fdee8e15a31ebbfbfadf8d1e
Harshavardhan1421 <harsha211220@gmail.com>
2024-10-04 19:52:17 +0530

    champions_leauge added

---
+++ champions_leauge.py
@@ -0,0 +1,6 @@
+def func():
+    print("real madrid has won 15 titles in championsleauge")
+    print("bayern munich has won 5 titles in championsleauge")
+    print("Ac milan has won 6 titles in championsleauge")
+    print("FC Barcelona has won 5 titles in championsleauge")
+func()
