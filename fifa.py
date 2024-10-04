57357895e503dd2342fa9d7d6cf40fdb4eca3ff8
Harshavardhan1421 <harsha211220@gmail.com>
2024-10-04 19:47:05 +0530

    brazil added in fifa

--- fifa.py
+++ fifa.py
@@ -1,5 +1,6 @@
 def func():
     print("fifa is a most popular sport in world")
-    print('germany has won fifa worldcup 5 times')
+    print('brazil has won fifa worldcup 5 times')
     print('Argentina has won fifa worldcup 3 times')
+    print('germany has won fifa worldcup 4 times')
 func()
